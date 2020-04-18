f423036515ed6a419770b19dea57be2341964fc3
Chibuisi Enyia <enyia21@gmail.com>
2020-04-18 10:59:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
-
+def my_collect(array)
+   if block_given?
+     i = 0
+     collection = []
+     while i < array.length
+        collection << yield(array[i]) #hops out of code into:  do|lang| lang.upcase end block
+        i +=1
+     end
+     collection
+   else
+   end
+end
